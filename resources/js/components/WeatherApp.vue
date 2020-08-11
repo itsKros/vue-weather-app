72788e89750852d5e85745df718845f84624764a
itsKros <kroshan197@gmail.com>
2020-08-11 18:39:11 +0530

    Laravel Mix, Vue, Tailwind configured

---
+++ resources/js/components/WeatherApp.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        Example
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
